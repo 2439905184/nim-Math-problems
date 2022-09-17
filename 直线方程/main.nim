7913a3ae63968dfa9dba51f1971753757333334d
2439905184 <suchenglong018@126.com>
2022-09-17 09:18:19 +0800

    🐞 fix: 修复运行报错

--- 直线方程/main.nim
+++ 直线方程/main.nim
@@ -1,19 +1,13 @@
 import strutils
 import strformat
-#[
+
 type
-  方程系数 = object
-    A,B,C: int
-]#
-type
-  point = object
+  Point = object
     x,y: int
 
 # 已知两点求K
-proc get_k(p1,p2:point):float =
+proc get_k(p1,p2:Point):float =
   var k = (p2.y - p1.y) / (p2.x - p1.x)
   return k
-
-# 暂时不能是负数参数 报错： Error: attempting to call routine: 'point'
-echo get_k(point(x:0,y:3),point(x:2,y:0))
-#var p1 = point()
+# 如果报错 需要这样写: Point(x: 0,y: 0 )
+echo get_k(Point(x:0,y:3),Point(x:2,y:0))
