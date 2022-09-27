9c266e857a17cfdb2ec88226e686d56026af3f4f
2439905184 <suchenglong018@126.com>
2022-09-27 19:58:10 +0800

    ✨ feat: 添加等差数列例题2

--- 数列/等差数列.nim
+++ 数列/等差数列.nim
@@ -10,5 +10,21 @@ proc answer1():float =
   var a7 = a1 + 6 * 公差
   return a7
 
-# 例题2：
+# 例题2：a5=-1,a8=2 求a1与d
+proc answer2():seq[float] = 
+  const a5 = -1
+  const a8 = 2
+  #[a5 = a1 + 4d
+  a8 = a1 + 7d ]#
+  var 三倍公差 = 3
+  var d = 3 / 三倍公差
+  #a1 + 四倍公差 = -1
+  #a1 + d * 4 = -1
+  var a1 = -1 - 4 * d
+  var mresult:seq[float] = @[]
+  mresult.add(a1)
+  mresult.add(d)
+  return mresult
+
 echo answer1()
+echo answer2()
